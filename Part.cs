e7934b675af2fdf7c82c0f0574191ec7444cb920
riverbobby <51435795+riverbobby@users.noreply.github.com>
2021-02-05 21:38:16 -0800

    Filled in BList

--- Part.cs
+++ Part.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace JustinTownleySoftwareI
 {
-    class Part
+    abstract class Part
     {
+        public int PartID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public static int partCount;
+
+        public Part(string n)
+        {
+            PartID = partCount++;
+            Name = n;
+        }
+
+        public Part(int partID, string n)
+        {
+            PartID = partID;
+            Name = n;
+        }
     }
 }
